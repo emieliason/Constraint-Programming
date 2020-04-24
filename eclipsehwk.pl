904562df0ea5793903b406f6b37f96b2c46cdfd4
kobytseng <55365350+kobytseng@users.noreply.github.com>
2020-04-23 19:31:08 -0700

    Add files via upload

--- eclipsehwk.pl
+++ eclipsehwk.pl
@@ -1,12 +1,5 @@
 /* Koby Tseng, Sergei Bondarenko, Emi Eliason */
 
-/* Notes from office hours session
-– Consolidate all problems to one method
-- Problem 7: Write out every variable
-- 
-
-*/
-
 :- lib(lists).
 :- lib(fd).
 :- lib(fd_search).
@@ -42,6 +35,9 @@ problem1Print(Teas) :-
       /* Problem 2 */
 
 problem2(Barrels) :-
+  problem2Setup(Barrels).
+
+problem2Setup(Barrels) :-
   Barrels = [Wine1, Wine2, Wine3, Wine4, Wine5, Beer],
   Barrels #:: [15, 19, 31, 20, 16, 18],
   alldifferent(Barrels),
@@ -76,9 +72,8 @@ problem3Solve(Digits) :-
   labeling(Digits).
 
 problem3Maximize(Digits, Product) :-
-  Product1 #= -1 * Product,
   /* Can't figure out why -Product doesn't work. */
-  minimize(labeling(Digits), Product1).
+  minimize(labeling(Digits), Product).
 
 problem3Print(Digits, Product) :-
   Digits = [Num1Dig1, Num1Dig2, Num1Dig3, Num2Dig1, Num2Dig2, Num3Dig1, Num3Dig2, Num4Dig1, Num4Dig2],
@@ -171,7 +166,7 @@ problem6(Planets) :-
 
 problem7(Casks) :-
   N1 = [N1f , N1tq, N1h, N1q, N1e],
-  N2 = [N1f , N2tq, N2h, N2q, N2e],
+  N2 = [N2f , N2tq, N2h, N2q, N2e],
   N3 = [N3f , N3tq, N3h, N3q, N3e],
   N4 = [N4f , N4tq, N4h, N4q, N4e],
   N5 = [N5f , N5tq, N5h, N5q, N5e],
@@ -240,4 +235,91 @@ problem7(Casks) :-
   Totals = [N1T, N2T, N3T, N4T, N5T],
   alldifferent(Totals),
 
-  labeling(Casks).
+  labeling(N1),
+  labeling(N2),
+  labeling(N3),
+  labeling(N4),
+  labeling(N5).
+
+problem8(Grid):-
+
+	Grid = [S11, S12, S13, S14, S15, S16, S17, S18,
+  S21, S22, S23, S24, S25, S26, S27, S28,
+	S31, S32, S33, S34, S35, S36, S37, S38,
+	S41, S42, S43, S44, S45, S46, S47, S48,
+	S51, S52, S53, S54, S55, S56, S57, S58,
+	S61, S62, S63, S64, S65, S66, S67, S68,
+	S71, S72, S73, S74, S75, S76, S77, S78,
+	S81, S82, S83, S84, S85, S86, S87, S88],
+
+	Grid :: [0,1],
+  1 * S31 + 2 * S32 + 3 * S33 + 4 * S34 + 5 * S35 + 6 * S36 + 7 * S37 + 8 * S38 #= 5,
+	1 * S41 + 2 * S42 + 3 * S43 + 4 * S44 + 5 * S45 + 6 * S46 + 7 * S47 + 8 * S48 #= 10,
+	1 * S51 + 2 * S52 + 3 * S53 + 4 * S54 + 5 * S55 + 6 * S56 + 7 * S57 + 8 * S58 #= 29,
+	1 * S71 + 2 * S72 + 3 * S73 + 4 * S74 + 5 * S75 + 6 * S76 + 7 * S77 + 8 * S78 #= 26,
+	1 * S11 + 2 * S21 + 3 * S31 + 4 * S41 + 5 * S51 + 6 * S61 + 7 * S71 + 8 * S81 #= 6,
+	1 * S12 + 2 * S22 + 3 * S32 + 4 * S42 + 5 * S52 + 6 * S62 + 7 * S72 + 8 * S82 #= 15,
+	1 * S13 + 2 * S23 + 3 * S33 + 4 * S43 + 5 * S53 + 6 * S63 + 7 * S73 + 8 * S83 #= 28,
+	1 * S14 + 2 * S24 + 3 * S34 + 4 * S44 + 5 * S54 + 6 * S64 + 7 * S74 + 8 * S84 #= 27,
+	1 * S15 + 2 * S25 + 3 * S35 + 4 * S45 + 5 * S55 + 6 * S65 + 7 * S75 + 8 * S85 #= 30,
+	1 * S16 + 2 * S26 + 3 * S36 + 4 * S46 + 5 * S56 + 6 * S66 + 7 * S76 + 8 * S86 #= 28,
+	1 * S17 + 2 * S27 + 3 * S37 + 4 * S47 + 5 * S57 + 6 * S67 + 7 * S77 + 8 * S87 #= 6,
+	1 * S18 + 2 * S28 + 3 * S38 + 4 * S48 + 5 * S58 + 6 * S68 + 7 * S78 + 8 * S88 #= 30,
+
+	labeling(Grid).
+
+problem9(Grid) :-
+    Grid = [S11, S12, S13, S14,
+            S21, S22, S23, S24,
+            S31, S32, S33, S34,
+            S41, S42, S43, S44],
+
+    Sum1 #= S11 + S12 + S13 + S14 + S21 + S31 + S41,
+    Sum1 #= (A*2),
+
+    Sum2 #= S11 + S12 + S13 + S14 + S22 + S32 + S42,
+    Sum2 #= (B*2),
+
+    Sum3 #= S11 + S12 + S13 + S14 + S23 + S33 + S43,
+    Sum3 #= (C*2),
+
+    Sum4 #= S11 + S12 + S13 + S14 + S24 + S34 + S44,
+    Sum4 #= (D*2) + 1,
+
+    Sum5 #= S21 + S22 + S23 + S24 + S11 + S31 + S41,
+    Sum5 #= (E*2),
+
+    Sum6 #= S21 + S22 + S23 + S24 + S12 + S32 + S42,
+    Sum6 #= (F*2),
+
+    Sum7 #= S21 + S22 + S23 + S24 + S13 + S33 + S43,
+    Sum7 #= (G*2),
+
+    Sum8 #= S21 + S22 + S23 + S24 + S14 + S34 + S44,
+    Sum8 #= (H*2) + 1,
+
+    Sum9 #= S31 + S32 + S33 + S34 + S11 + S21 + S41,
+    Sum9 #= (I*2) + 1,
+
+    Sum10 #= S31 + S32 + S33 + S34 + S12 + S22 + S42,
+    Sum10 #= (J*2),
+
+    Sum11 #= S31 + S32 + S33 + S34 + S13 + S23 + S43,
+    Sum11 #= (K*2) + 1,
+
+    Sum12 #= S31 + S32 + S33 + S34 + S14 + S24 + S44,
+    Sum12 #= (L*2),
+
+    Sum13 #= S41 + S42 + S43 + S44 + S11 + S21 + S31,
+    Sum13 #= (M*2) + 1,
+
+    Sum14 #= S41 + S42 + S43 + S44 + S12 + S22 + S32,
+    Sum14 #= (N*2),
+
+    Sum15 #= S41 + S42 + S43 + S44 + S13 + S23 + S33,
+    Sum15 #= (O*2),
+
+    Sum16 #= S41 + S42 + S43 + S44 + S14 + S24 + S34,
+    Sum16 #= (P*2),
+
+    labeling(Grid).
